9293312c31383d303081bef5073fa8da8805ed62
Nahom <zerihunnahom@gmail.com>
2023-07-12 01:36:50 +0300

    Update Greeting component

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreeting } from '../redux/features/greeting/greetingSlice';
 
 const Greeting = () => {
+    const {greeting, isLoading, error} = useSelector((store) => store.greeting);
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getGreeting());
+    },[dispatch]);
+
     return (
-        <h1>Greeting!!</h1>
+        <h1>{greeting.title}</h1>
     )
 }
 export default Greeting
