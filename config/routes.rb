a1bc6b75a07c9f6ee071fd8e248fad0ae6b70fab
Nahom <zerihunnahom@gmail.com>
2023-07-12 00:27:52 +0300

    Add route to greeting as an api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'root/index'
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
